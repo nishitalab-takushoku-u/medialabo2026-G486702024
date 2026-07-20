fe685013f3c5cf410a8880a3b2002432a1ec9238
MurataShun <248670@st.takushoku-u.ac.jp>
2026-07-20 15:59:05 +0900

    第6回課題

--- weather.js
+++ weather.js
@@ -13,8 +13,6 @@ function print(data) {
   console.log("風速: "+data.wind.speed);
   console.log("風向: "+data.wind.deg);
 }
-let b = document.querySelector('button#btn');
-b.addEventListener('click', showSelectResult);
 
 function showSelectResult() {
     let s = document.querySelector('#city');
@@ -83,23 +81,49 @@ function printDom(data) {
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
-
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+    let s = document.querySelector('#city');
+    let idx = s.selectedIndex;
 
+    let os = s.querySelectorAll('option');
+    let o = os.item(idx);
+
+    let id = o.getAttribute('value');
+    let url='https://www.nishita-lab.org/web-contents/jsons/openweather/' + id +'.json';
+
+
+ // 通信開始
+  axios.get(url)
+    .then(showResult)   // 通信成功
+    .catch(showError)   // 通信失敗
+    .then(finish);      // 通信の最後の処理
 }
+
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
 
+ // サーバから送られてきたデータを出力
+ let data = resp.data;
+
+ // data が文字列型なら，オブジェクトに変換する
+ if (typeof data === 'string') {
+     data = JSON.parse(data);
+ }
+     printDom(data);
 }
+
 
 // 課題6-1: 通信エラーが発生した時の処理
 function showError(err) {
     console.log(err);
 }
+
 
 // 課題6-1: 通信の最後にいつも実行する処理
 function finish() {
@@ -110,50 +134,3 @@ function finish() {
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
